d917d5e9fd48a49e8aba5756c78893d679c63c02
nikolai-third <k.pobedin2004@gmail.com>
2024-02-18 14:21:12 +0300

    lab1 stage2

---
+++ laba1/stage2/1.py
@@ -0,0 +1,26 @@
+import gmsh
+import sys
+import os
+import math
+
+gmsh.initialize()
+
+gmsh.model.add("t2")
+
+path = os.path.dirname(os.path.abspath(__file__))
+gmsh.merge(os.path.join(path, "dart.stl"))
+
+gmsh.model.mesh.classifySurfaces(40 * math.pi/180, True, False, 180 * math.pi /180)
+
+s = gmsh.model.getEntities(2)
+l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
+#gmsh.model.geo.addVolume([l])
+
+gmsh.model.geo.synchronize()
+
+gmsh.model.mesh.generate(3)
+
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
+
+gmsh.finalize()
